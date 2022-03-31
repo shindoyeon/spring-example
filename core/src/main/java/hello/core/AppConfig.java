179e914f42fe3b7e94583d3cfd57c0818aa14eeb
shindoyeon <dyeon7310@gmail.com>
2022-04-01 02:14:26 +0900

    스프링 컨테이너 생성 및 빈 등록, 스프링 빈 조회 테스트 코드 작성

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,31 +2,40 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig  {
     //역할
+    @Bean //스프링컨테이너에 등록됨
     public MemberService memberService(){
+        //이 반환된 객체를 스프링 컨테이너에 등록 -> 등록된 객체를 스프링 빈 객체라고 한다
         return new MemberServiceImpl(memberRepository());
     }
 
-    //구현
-    private MemberRepository memberRepository() {
+    //구현(나중에 변경할 일이 있을 경우 구현부만 수정하면 된다.)
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     //역할
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     //구현
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,21 +7,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.swing.plaf.metal.MetalMenuBarUI;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
         //MemberService memberService = new MemberServiceImpl();
         //OrderService orderService = new OrderServiceImpl();
+
+        //ApplicationContext를 스프링 컨데이너라고 한다(@Configuration이 붙은 AppConfig를 설정 정보로 사용)
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberID = 1L;
         Member member = new Member(memberID, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberID, "itemA", 10000);
+        Order order = orderService.createOrder(memberID, "itemA", 20000);
 
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
